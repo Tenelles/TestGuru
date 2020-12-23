9c41526df543fcccd6f11b9251ba1337b96b10b4
tenelles <tenelles@yandex.ru>
2020-12-23 21:03:19 +0300

    add base tables and null constraint

---
+++ db/migrate/20201223173446_add_tests_title_and_category_id_null_constraint.rb
@@ -0,0 +1,6 @@
+class AddTestsTitleAndCategoryIdNullConstraint < ActiveRecord::Migration[6.0]
+  def change
+    change_column_null(:tests,:title,false)
+    change_column_null(:tests,:category_id,false)
+  end
+end
