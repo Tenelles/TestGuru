6188c71951f069afa66bd901e76056a490790a9e
tenelles <tenelles@yandex.ru>
2020-12-28 19:23:10 +0300

    fixed commit mistake

--- db/migrate/20201223172257_create_tests.rb
+++ db/migrate/20201223172257_create_tests.rb
@@ -3,8 +3,8 @@ class CreateTests < ActiveRecord::Migration[6.0]
     create_table :tests do |t|
       t.string :title, null: false
       t.integer :level, default: 1
-      t.references :creator, index: { name: "index_tests_on_user_id" }, null: false
-      t.references :category, null: false
+      t.references :user, null: false, foreign_key: true
+      t.references :category, null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,46 +10,54 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_23_173033) do
-
-  create_table "answers", force: :cascade do |t|
-    t.string "title", null: false
-    t.boolean "correct", default: false, null: false
-    t.integer "question_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["question_id"], name: "index_answers_on_question_id"
+ActiveRecord::Schema.define(version: 20_201_228_153_325) do
+  create_table 'answers', force: :cascade do |t|
+    t.string 'title', null: false
+    t.boolean 'correct', default: false, null: false
+    t.integer 'question_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['question_id'], name: 'index_answers_on_question_id'
   end
 
-  create_table "categories", force: :cascade do |t|
-    t.string "title", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'categories', force: :cascade do |t|
+    t.string 'title', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  create_table "questions", force: :cascade do |t|
-    t.string "title", null: false
-    t.integer "category_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["category_id"], name: "index_questions_on_category_id"
+  create_table 'questions', force: :cascade do |t|
+    t.string 'title', null: false
+    t.integer 'category_id', null: false
+    t.integer 'test_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['category_id'], name: 'index_questions_on_category_id'
+    t.index ['test_id'], name: 'index_questions_on_test_id'
   end
 
-  create_table "tests", force: :cascade do |t|
-    t.string "title", null: false
-    t.integer "level", default: 1
-    t.integer "creator_id", null: false
-    t.integer "category_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["category_id"], name: "index_tests_on_category_id"
-    t.index ["creator_id"], name: "index_tests_on_user_id"
+  create_table 'tests', force: :cascade do |t|
+    t.string 'title', null: false
+    t.integer 'level', default: 1
+    t.integer 'user_id', null: false
+    t.integer 'category_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['category_id'], name: 'index_tests_on_category_id'
+    t.index ['user_id'], name: 'index_tests_on_user_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'name', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
+  add_foreign_key 'answers', 'questions'
+  add_foreign_key 'questions', 'categories'
+  add_foreign_key 'questions', 'tests'
+  add_foreign_key 'test_passings', 'tests'
+  add_foreign_key 'test_passings', 'users'
+  add_foreign_key 'tests', 'categories'
+  add_foreign_key 'tests', 'users'
 end
