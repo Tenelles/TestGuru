5ce94baef8a094a5ac7d0ea013ad0257d316b892
tenelles <tenelles@yandex.ru>
2021-01-20 20:00:56 +0300

    renamed and optimized find_by_category

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,6 @@
 class Test < ApplicationRecord
-  def self.find_by_category(category_name)
-    res = []
-    joined_table = joins('tests JOIN categories ON tests.category_id = categories.id').where(
-      'categories.title = ?', category_name).find_each { |test| res.push(test.title) }
-    res.sort! { |a, b| b <=> a}
+  def self.get_tests_titles_with_category(title)
+    joins('JOIN categories ON tests.category_id = categories.id').where(
+      categories: {title: title}).pluck(:title)
   end
 end
