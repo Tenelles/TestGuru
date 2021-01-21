f2f383b1da24ac9437c89b5ee8001a7e2fffe468
tenelles <tenelles@yandex.ru>
2021-01-21 11:44:50 +0300

    style changes

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,7 @@
 class Test < ApplicationRecord
-  def self.get_tests_titles_with_category(title)
-    joins('JOIN categories ON tests.category_id = categories.id').where(
-      categories: {title: title}).pluck(:title)
+  def self.tests_titles_with_category(title)
+    joins('JOIN categories ON tests.category_id = categories.id')
+      .where(categories: {title: title})
+      .pluck(:title)
   end
 end
