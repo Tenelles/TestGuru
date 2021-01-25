5b0864593cad6f1dffec31e13c5f80085527d10e
tenelles <tenelles@yandex.ru>
2021-01-25 19:29:08 +0300

    add scopes

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,11 +1,22 @@
 class Test < ApplicationRecord
   belongs_to :category
-  belongs_to :author, class_name: "User",
-                      foreign_key: "user_id"
+  belongs_to :author, class_name: 'User',
+                      foreign_key: 'user_id'
 
   has_many :questions, dependent: :destroy
   has_many :test_passings, dependent: :destroy
   has_many :users, through: :test_passings
+
+  scope :with_difficulty, -> (difficulty_level) {
+    case difficulty_level
+    when :easy
+      where(level: 0..1)
+    when :medium
+      where(level: 2..4)
+    when :hard
+      where(level: 5..Float::INFINITY)
+    end
+  }
 
   def self.titles(category_title)
     joins(:category)
