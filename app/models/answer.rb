899de440ac68e86d6ae6e8d8307f01cb26e528db
tenelles <tenelles@yandex.ru>
2021-01-29 20:20:41 +0300

    answers count validation redone

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,6 +2,13 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :body, presence: true
+  validate :validation_count
 
   scope :correct, -> { where(correct: true) }
+
+  def validation_count
+      if question.answers.count > 4
+        errors.add(:answers_count, "can't be that much")
+      end
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -3,14 +3,5 @@ class Question < ApplicationRecord
 
   has_many :answers, dependent: :destroy
 
-
   validates :body, presence: true
-  validate :validation_answers_count
-
-  def validation_answers_count
-    if (answers.count >= 1 && answers.count <= 4)
-      errors.add(:answers_count, "can't be that much")
-    end
-  end
-
 end
