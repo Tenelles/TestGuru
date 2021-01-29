46bb3245301773f30593ca48787c1fa05003ac52
tenelles <tenelles@yandex.ru>
2021-01-29 19:12:59 +0300

    added positive test level validation

--- app/models/test.rb
+++ app/models/test.rb
@@ -8,7 +8,7 @@ class Test < ApplicationRecord
   has_many :users, through: :test_passings
 
   validates :title, presence: true
-  validates :level, numericality: { only_integer: true }
+  validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :title, uniqueness: { scope: :level }
 
   scope :easy, -> { where(level: 0..1) }
