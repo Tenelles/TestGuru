3e782d6b94fce9f972966db9d98ae148dd3583ed
tenelles <tenelles@yandex.ru>
2021-02-01 19:44:59 +0300

    renamed validation_count->validation_answers_count + fixed bugs

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -6,8 +6,8 @@ class Answer < ApplicationRecord
 
   scope :correct, -> { where(correct: true) }
 
-  def validation_count
-      if question.answers.count > 4
+  def validation_answers_count
+      if question.answers.count > 3
         errors.add(:answers_count, "can't be that much")
       end
   end
