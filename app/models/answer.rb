cbde3863de28c2edb64820b16ae38a13c221b046
tenelles <tenelles@yandex.ru>
2021-02-01 19:42:34 +0300

    now tests validates on create

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,7 +2,7 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :body, presence: true
-  validate :validation_count
+  validate :validation_count, on: :create
 
   scope :correct, -> { where(correct: true) }
 
