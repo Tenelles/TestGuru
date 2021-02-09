6f3fd71d71bf867b4c8adad246220ddd148aa60b
tenelles <tenelles@yandex.ru>
2021-02-09 21:16:49 +0300

    add routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/tests/:id/questions', to: 'questions#index'
+  get '/questions/:id', to: 'questions#show'
+
+  post '/questions', to: 'questions#create'
+
+  delete '/questions/:id', to: 'questions#destroy'
 end
