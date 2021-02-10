f70a4c306b3b7a6da059f0bcbff6f3694ede2554
tenelles <tenelles@yandex.ru>
2021-02-10 22:59:12 +0300

    added base logic

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,37 @@
 class QuestionsController < ApplicationController
+
+  def index
+    render json: { questions: Test.find(params[:id]).questions.pluck(:id, :body) }
+  end
+
+  def show
+    render json: { question: Question.find(params[:id]).inspect}
+  end
+
+  def new
+  end
+
+  def delete
+  end
+
+  def create
+    question = Question.new(question_params)
+    question.test_id = params[:id]
+    question.save
+
+    render json: { result: question.inspect}
+  end
+
+  def destroy
+    Question.find(params[:id]).destroy
+
+    render plain: 'Question was deleted'
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:body);
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
 
   get '/tests/:id/questions', to: 'questions#index'
   get '/questions/:id', to: 'questions#show'
+  get '/tests/:id/questions/new', to: 'questions#new'
+  get '/questions/:id/delete', to: 'questions#delete'
 
-  post '/questions', to: 'questions#create'
-
-  delete '/questions/:id', to: 'questions#destroy'
+  post '/tests/:id/questions/create', to: 'questions#create'
+  post '/questions/:id/destroy', to: 'questions#destroy'
 end
