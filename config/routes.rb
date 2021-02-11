638fb5f5f9506b4ab629110ce8cd1c88e1018930
tenelles <tenelles@yandex.ru>
2021-02-11 17:24:38 +0300

    now using "resources" routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
 
-  get '/tests/:id/questions', to: 'questions#index'
-  get '/questions/:id', to: 'questions#show'
-  get '/tests/:id/questions/new', to: 'questions#new'
-  get '/questions/:id/delete', to: 'questions#delete'
-
-  post '/tests/:id/questions/create', to: 'questions#create'
-  post '/questions/:id/destroy', to: 'questions#destroy'
+  resources :tests do
+    resources :questions, shallow: true
+  end
+  
 end
